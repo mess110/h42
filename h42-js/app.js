bfddf0250e3f8744420ba0cc218adc619f3a6a63
Cristian Messel <mess110@gmail.com>
2013-01-20 00:36:09 +0000

    control pin13 from web ui

--- h42-js/app.js
+++ h42-js/app.js
@@ -1,7 +1,12 @@
 var express = require('express')
   , app = express()
   , server = require('http').createServer(app)
-  , io = require('socket.io').listen(server);
+  , io = require('socket.io').listen(server)
+  , firmata = require('firmata');
+
+var board = new firmata.Board('/dev/ttyUSB0', function() {
+  board.digitalWrite(13, board.LOW);
+});
 
 app.configure(function() {
   app.use('/public', express.static(__dirname + '/public'));
@@ -14,7 +19,13 @@ app.get('/', function (req, res) {
 
 io.sockets.on('connection', function (socket) {
   socket.on('slide', function (data) {
-    console.log(data);
+    if (data['id'] === 'leftEngine') {
+      if (data['value'] > 122) {
+        board.digitalWrite(13, board.HIGH);
+      } else {
+        board.digitalWrite(13, board.LOW);
+      }
+    }
   });
 });
 
