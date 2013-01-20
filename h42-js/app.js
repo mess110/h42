8ed66bba61473454285f23c8dc0c5d1bc26bc367
Cristian Messel <mess110@gmail.com>
2013-01-20 13:09:24 +0000

    config + working robot

--- h42-js/app.js
+++ h42-js/app.js
@@ -2,11 +2,18 @@ var express = require('express')
   , app = express()
   , server = require('http').createServer(app)
   , io = require('socket.io').listen(server)
-  , firmata = require('firmata');
+  , firmata = require('firmata')
+  , config = require('./config');
 
-var board = new firmata.Board('/dev/ttyUSB0', function() {
-  board.digitalWrite(13, board.LOW);
-});
+var board = new firmata.Board(config.h42.port, function() {});
+
+function digitalHigh(pin) {
+  board.digitalWrite(pin, board.HIGH);
+}
+
+function digitalLow(pin) {
+  board.digitalWrite(pin, board.LOW);
+}
 
 app.configure(function() {
   app.use('/public', express.static(__dirname + '/public'));
@@ -17,16 +24,38 @@ app.get('/', function (req, res) {
   res.render('index');
 });
 
+app.get('/ping', function(req, res) {
+  res.end('pong');
+});
+
 io.sockets.on('connection', function (socket) {
   socket.on('slide', function (data) {
     if (data['id'] === 'leftEngine') {
-      if (data['value'] > 122) {
-        board.digitalWrite(13, board.HIGH);
+      val = data['value'];
+      if (val >= 50) {
+        board.digitalWrite(9, 0);
+        board.digitalWrite(6, data['value']);
+      } else if (val < -50) {
+        board.digitalWrite(9, data['value']);
+        board.digitalWrite(6, 0);
       } else {
-        board.digitalWrite(13, board.LOW);
+        board.digitalWrite(9, 0);
+        board.digitalWrite(6, 0);
+      }
+    } else if (data['id'] === 'rightEngine') {
+      val = data['value'];
+      if (val >= 50) {
+        board.digitalWrite(3, 0);
+        board.digitalWrite(5, data['value']);
+      } else if (val < -50) {
+        board.digitalWrite(3, data['value']);
+        board.digitalWrite(5, 0);
+      } else {
+        board.digitalWrite(3, 0);
+        board.digitalWrite(5, 0);
       }
     }
   });
 });
 
-server.listen(8080);
+server.listen(config.server.port);
---
+++ h42-js/config.js
@@ -0,0 +1,11 @@
+var config = {}
+
+config.server = {
+  port: 8080
+}
+
+config.h42 = {
+  port: '/dev/ttyACM0'
+}
+
+module.exports = config
