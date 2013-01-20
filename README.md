64efd50d9a5b8f8b9e9ccb762154991ac6eae375
Cristian Messel <mess110@gmail.com>
2013-01-20 00:13:20 +0000

    firmata working from node

---
+++ README.md
@@ -0,0 +1,4 @@
+node -v
+0.8.16
+
+https://dl.dropbox.com/s/sh3f72vtqcpekv9/index.html
---
+++ camera.sh
@@ -0,0 +1,3 @@
+sudo apt-get install libv4l-0
+wget -O mjpg-streamer-rpi.tar.gz http://www.bobtech.ro/get?download=36:mjpg-streamer-rpi
+tar -zxvf mjpg-streamer-rpi.tar.gz
